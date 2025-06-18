58c1934a3750c2fec86fb2c4b875bdab3cbddf72
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-06-18 10:24:56 -0400

    create fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -3,7 +3,7 @@ const needle = require("needle");
 const fetchMyIP = (callback) => {
   needle.get("https://api.ipify.org?format=json", (err, response, body) => {
 
-    if (err) callback(err, null);
+    if (err) return callback(err, null);
 
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
@@ -17,5 +17,24 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = {fetchMyIP};
+const fetchCoordsByIP = (ip, callback) => {
+  needle.get(`http://ipwho.is/${ip}`, (err, response, body) => {
+    
+    if (err) return callback(err, null);
+
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    const coords = {latitude: body.latitude, longitude: body.longitude};
+
+    callback(null, coords);
+
+  });
+};
+
+
+
+module.exports = {fetchMyIP, fetchCoordsByIP};
 
